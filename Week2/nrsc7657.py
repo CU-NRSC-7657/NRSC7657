9e69e2ff261def0f97fd84b2d04c2cb18e70abb7
daniel denman <danieljdenman@gmail.com>
2023-06-26 11:22:50 -0600

    week 3 python materials

---
+++ Week2/nrsc7657.py
@@ -0,0 +1,26 @@
+class ThingWeMade:
+    def __init__(self):
+        self.thing='neuron'
+        
+        self.start()
+    
+    def start(self):
+        self.thing2='neuron'
+        
+    def fire(self):
+        self.spike = self.thing+' fires an action potential'
+        print(self.spike)
+        
+    def fire_a_thing(self,input_string):
+        self.spike = self.thing+' fires an '+input_string
+        print(self.spike)
+
+def startswith(_string='take',string_to_check='dan'):
+    out = False
+    for character in range(len(string_to_check)):
+        if string_to_check[character] == _string[character]:
+            out = True
+        else: 
+            out = False
+            break
+    return out
